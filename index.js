7763db79f99a800ff100328e257477579428ee2f
Akshat Gupta <akshat.gupta.ts@gmail.com>
2017-04-13 17:07:30 +0530

    Minor fix

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 var connector = new builder.ChatConnector({
-    //appId: process.env.MICROSOFT_APP_ID,
-    //appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 var model = process.env.LUIS;
